84e905a40c84a0236f67d1f1e78893462bf3c402
bcritt1 <bcritt@stanford.edu>
2023-12-05 14:45:57 -0800

    add and remove files

--- sentiment.py
+++ sentiment.py
@@ -3,12 +3,12 @@
 # Import packages
 import os
 import re
-import json
+import ssl
 from transformers import pipeline
 import pandas as pd
 import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
-import ssl
+from nltk.tokenize import sent_tokenize
+
 
 
 # This may or may not be necessary for you. Gives python permission to access the internet so we can download 
@@ -22,8 +22,9 @@ else:
 nltk.download('punkt')
 # Read in corpus
 user = os.getenv('USER')
-corpusdir = '/Users/bcritt/Documents/StanfordProjects/Corpora/Emerson/emerson/'
+#corpusdir = '/Users/bcritt/Documents/StanfordProjects/Corpora/Emerson/emerson/'
 #corpusdir = '/farmshare/learning/data/emerson/'.format(user)
+corpusdir = '/home/users/{}/corpora/test_corpus/'.format(user)
 corpus = []
 for infile in os.listdir(corpusdir):
     with open(corpusdir+infile, errors='ignore') as fin:
@@ -44,11 +45,12 @@ sorpus = re.sub(r'(\\n[ \t]*)+', '', sorpus)
 sentences = sent_tokenize(sorpus)
 
 
-'''# Import language models and pipeline elements
-from transformers import AutoTokenizer, AutoModelForTokenClassification
-tokenizer = AutoTokenizer.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
-model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
-'''
+# Import language models and pipeline elements
+#from transformers import AutoTokenizer, AutoModelForTokenClassification
+#tokenizer = AutoTokenizer.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
+#model = AutoModelForTokenClassification.from_pretrained("Jean-Baptiste/roberta-large-ner-english")
+
+#SENTIMENT CLASSIFIER
 #pipe = pipeline(model="roberta-large-mnli")
 pipe = pipeline("text-classification")
 #pipe(sentences, vocab_size=30522, model_max_length=512, is_fast=True, padding_side='right', truncation_side='right')
@@ -56,15 +58,11 @@ pipe = pipeline("text-classification")
 model = pipe(sentences, max_length = 512, padding="max_length", truncation=True)
 print(model)
 print(len(model))
-
-'''
-# can be combined as
-classifier = pipeline('zero-shot-classification', model='roberta-large-mnli')
-candidate_labels = ['travel', 'nature', 'philosophy']
-model = classifier(corpus, candidate_labels, max_length = 512, padding="max_length", truncation=True)
-'''
-import pandas as pd
 df = pd.DataFrame(model)
-df.to_csv("foo.csv")
-#print(model)
-#print(len(model))
+df.to_csv("/scratch/users/{}/outputs/foo.csv".format(user))
+
+#GENRE CLASSIFIER
+#can be combined as
+#classifier = pipeline('zero-shot-classification', model='roberta-large-mnli')
+#candidate_labels = ['travel', 'nature', 'philosophy']
+#model = classifier(corpus, candidate_labels, max_length = 512, padding="max_length", truncation=True)
